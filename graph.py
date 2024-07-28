2066a7a225769bda20e731dbbd0ee71827fdcc71
Nathan King <nateking0345@gmail.com>
2024-07-28 10:41:38 -0400

    Finished graph data structure

--- graph.py
+++ graph.py
@@ -19,10 +19,23 @@ class Graph:
         graph.add_nodes_from(nodes)
 
         weights = defaultdict(int)
+        user_books = defaultdict(list)
+
         for i in range(len(self.reviews)):
             if self.reviews[i] == 5:
-                for j in range(i + 1, len(self.reviews))
-                if self.reviews[j]
+                user_books[self.user_ids[i]].append(self.book_ids[i]) #maps user id to the books they reviewed 5 stars
+
+        for books in user_books.values():
+            for i in range(len(books)):
+                for j in range(i + 1, len(books)):
+                    weights[(books[i], books[j])] += 1 # adds wei
+
+
+
+        for(book1, book2), weight in weights.items(): # gets the key value pair, which is tuple to int
+            graph.add_edge(book1, book2, weight=weight)
+
+
 
 
 
